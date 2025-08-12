bfc4a907ae47d52089cba48b2e77f0407f4f1894
Moira Andrews <moira.lee.andrews@gmail.com>
2025-08-12 08:54:14 -0700

    added test function to test an input directory

--- tests/test_load_file.py
+++ tests/test_load_file.py
@@ -1,15 +1,19 @@
-from SNFit.load_file import file_formatting
-from pathlib import Path
+from SNFit.load_file import load_dir
+import os
+import glob
 
-def test_file_formatting():
+def test_directory_add():
     '''
-    Test file_formatting() to make sure files are ingested correctly.
-    Should return a dictionary including the new filepath
+    Test load_dir() to make sure files are ingested correctly if given as a directory input.
+    Should return nothing if successful.
     '''
-    test_dir = (Path(__file__).parent / "test_files").resolve()
-    file_dict = file_formatting(str(test_dir))
+    test_dir = os.path.join(os.path.dirname(__file__), "test_files")
+    files = glob.glob(os.path.join(test_dir, '*'))
+    file_dict = load_dir(files)
     assert isinstance(file_dict, dict)
     assert len(file_dict) > 0
     for path in file_dict.values():
-        assert Path(path).exists()
+        assert os.path.exists(path)
 
+if __name__ == "__main__":
+    test_directory_add()
