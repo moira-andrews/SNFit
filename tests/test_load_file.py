365e28291a03d40253e190633aa5401b1b487a74
Moira Andrews <moira.lee.andrews@gmail.com>
2025-08-12 08:43:36 -0700

    added test files, update pyproject.toml to be executible from terminal

--- tests/test_load_file.py
+++ tests/test_load_file.py
@@ -1,2 +1,15 @@
-import SNFit.load_file as lf
+from SNFit.load_file import file_formatting
+from pathlib import Path
+
+def test_file_formatting():
+    '''
+    Test file_formatting() to make sure files are ingested correctly.
+    Should return a dictionary including the new filepath
+    '''
+    test_dir = (Path(__file__).parent / "test_files").resolve()
+    file_dict = file_formatting(str(test_dir))
+    assert isinstance(file_dict, dict)
+    assert len(file_dict) > 0
+    for path in file_dict.values():
+        assert Path(path).exists()
 
