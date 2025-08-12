ece50eab19f144f60e34ff468aa3d10a53880eaa
Moira Andrews <moira.lee.andrews@gmail.com>
2025-08-12 09:50:56 -0700

    adding tests for error columns

---
+++ tests/test_lightcurve.py
@@ -0,0 +1,25 @@
+import os
+import glob
+from SNFit.lightcurve import LightCurve
+
+def test_nan_lightcurve_object():
+    '''
+    Make Lightcurve object from the nan_test.txt file in the test directory.
+    Should remove the nan rows, leaving a df table of length 12.
+    '''
+    test_file = os.path.join(os.path.dirname(__file__), "test_files/nan_test.txt")
+    lc = LightCurve(test_file)
+    assert len(lc.df) == 12
+
+def test_delim_lightcurve_object():
+    '''
+    Make lightcurve object from the different_delim_test.txt file in test directory.
+    DF made should handle the different delimeters and spacing.
+    '''
+    test_file = os.path.join(os.path.dirname(__file__), "test_files/different_delim_test.txt")
+    lc = LightCurve(test_file)
+    assert not lc.df.empty
+
+if __name__=="__main__":
+    test_nan_lightcurve_object()
+    test_delim_lightcurve_object()
